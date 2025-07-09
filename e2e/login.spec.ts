c07df9a6874a9e75f3faf9a3f26572443f544bd1
Brandon Ting <brandonting@kkcompany.com>
2025-07-09 14:32:22 +0800

    remove unnecessary tests

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,6 +1,25 @@
 import { expect, test } from '@playwright/test';
 
-test('should navigate to the login page', async ({ page }) => {
+test('has title', async ({ page }) => {
+  await page.goto('/GDrive-Audio-Player/login');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Rsbuild App/);
+});
+
+test('login page has sign in button', async ({ page }) => {
+  await page.goto('/GDrive-Audio-Player/login');
+
+  // Expect the page to have a button with the text "Sign in with Google".
+  await expect(
+    page.getByRole('button', { name: 'Sign in with Google' }),
+  ).toBeVisible();
+});
+
+test('should navigate to the login page when user is not logged in', async ({
+  page,
+}) => {
   await page.goto('/');
   await expect(page.locator('h1')).toContainText('GDrive Audio Player');
+  await expect(page.locator('button')).toContainText('Sign in with Google');
 });
